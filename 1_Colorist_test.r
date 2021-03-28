277e135f3607931e35c5da5b06e62bcf1d097932
Ludovico Chieffallo <79975866+Ludovico-Chieffallo@users.noreply.github.com>
2021-03-29 00:15:30 +0200

    Update 1_Colorist_test.r

--- 1_Colorist_test.r
+++ 1_Colorist_test.r
@@ -1,17 +1,16 @@
 #Il pacchetto "colorist" (R) è stato creato per fornire ai ricercatori metodologie e opzioni in più per studiare e comunicare
 #informazioni sulla distribuzione della fauna selvatica nello spazio e nel tempo. 
-#Per fare questo "colorist"
+#Per fare questo "colorist" utilizza immagini Rasterstack che descrivono le distribuzioni della fauna selvatica, le elabora
+#e le collega alle tavolozze HCL in modi specifici. le visualizzazioni risultanti consentono agli spettatori di confrontare
+#in modo significativo i valori di occorrenza, abbondanza, o densità nello spazio e nel tempo
 
+#Cos'è la tavolozza HCL? Lo spazio colore Hue-Chroma-Luminance (HCL) è un'alternativa ad altri spazi colore come RGB, HSV e così via. 
+# Ogni colore all'interno dello spazio colore HCL è definito da una tripletta di valori. Le dimensioni sono:
 
-
-
-# Per farlo in modo efficace, colorista prende le informazioni da una
-#pila di raster che descrivono le distribuzioni della fauna selvatica, le elabora e le collega
-#alle tavolozze HCL (tonalità-croma-luminanza) in modi specifici. Le visualizzazioni risultanti
-#consentono agli spettatori di confrontare in modo significativo i valori di occorrenza,
-#abbondanza o densità nello spazio e nel tempo.
-
-
+#H ue: definisce il colore (tonalità)
+#C hroma: definisce il colore (saturazione o intensità del colore)
+#L uminance: definisce la luminosità
+#(https://hclwizard.org/images//hclscheme_pic0.png)
 
 #Il flusso di lavoro di base per colorist è il seguente:
 
@@ -20,15 +19,16 @@
 #3.)Mappa : gli utenti combinano metriche e una tavolozza per mappare le distribuzioni in una serie di piccoli multipli o in una singola mappa.
 #4.)Legenda : gli utenti generano una legenda per accompagnare la loro mappa.
 
-
-#La differenza principale tra RasterBrick e Rasterstack è che a RasterBrick può essere collegato solo a un singolo file (multistrato, come immagini satellitari). Al contrario, un RasterStack può essere formato da file separati e / o da pochi livelli ("bande") da un singolo file.
+#Prima di iniziare definiamo la differenza tra RasterBrick (usato in precedenza) e Rasterstack.
+#La differenza principale tra RasterBrick e Rasterstack è che a RasterBrick può essere collegato solo a un singolo file (multistrato, come immagini satellitari).
+#Al contrario, un RasterStack può essere formato da file separati e / o da pochi livelli ("bande") da un singolo file.
 
 install.packages("colorist")
 library(colorist)
 
 
-#Mappiamo una distribuzione delle specie nel ciclo annuale
-#carichiamo un esempio
+#Per iniziare con un esempio mappiamo una distribuzione di specie in un ciclo annuale
+#Carichiamo l'esempio usando la funzione data
 
 data("fiespa_occ")
 fiespa_occ
@@ -56,6 +56,12 @@ print(met1)
 #mes      :       jan,       feb,       mar,       apr,       may,       jun,       jul,       aug,       sep,       oct,       nov,       dec 
 #in values :         0,         0,         0,         0,         0,         0,         0,         0,         0,         0,         0,         0 
 #ax values : 0.9835264, 0.9529886, 0.9208400, 0.8991927, 0.8984586, 0.8922251, 0.8785749, 0.9063431, 0.8974113, 0.9096173, 0.9397971, 1.0000000 
+
+
+
+
+
+
 
 
 
@@ -90,3 +96,4 @@ map_multiples(met2, pal2, ncol = 2,lambda_i = -5,labels = names(elephant_ud))
 met2_distt<-metrics_distill(elephant_ud)
 map_single(met2_distt,pal2,lambda_i = -5)
 legend_set(pal2, group_labels = names(elephant_ud))
+
