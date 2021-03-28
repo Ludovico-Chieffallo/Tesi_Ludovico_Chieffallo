f873421637d1792e659db2877e4cb28862d4aaab
Ludovico Chieffallo <79975866+Ludovico-Chieffallo@users.noreply.github.com>
2021-03-28 14:49:15 +0200

    Update 1_Colorist_test.r

--- 1_Colorist_test.r
+++ 1_Colorist_test.r
@@ -1,1 +1,23 @@
+install.packages("colorist")
+library(colorist)
+
+#load example data, field sparrow occurence probability
+
+data("fiespa_occ")
+fiespa_occ
+plot(fiespa_occ)
+dev.off()
+
+#calculate distribution metrics
+met<- metrics_distill(fiespa_occ)
+
+#generate hcl color palette
+
+pal<- palette_timecycle(fiespa_occ)
+
+#map
+map_single(met,pal,lambda_i = -2)
+
+#legend
+legend_timecycle(pal)
 
