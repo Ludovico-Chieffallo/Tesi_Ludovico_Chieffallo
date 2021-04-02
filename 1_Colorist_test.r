900a82bc1f838b041efdc2b042a780829be6bcc1
Ludovico Chieffallo <79975866+Ludovico-Chieffallo@users.noreply.github.com>
2021-04-02 21:00:06 +0200

    Update 1_Colorist_test.r

--- 1_Colorist_test.r
+++ 1_Colorist_test.r
@@ -117,11 +117,48 @@ legend_timecycle(pal, origin_label = "jan 1")
 
 
 #ESEMPIO 2: MAPPARE IL COMPORTAMENTO INDIVIDUALE NEL TEMPO
-#Qui, coloristesploriamo come un individuo Fisher ( Pekania pennanti ) che vive nello stato di New York si è spostato nel suo ambiente locale per un periodo di nove notti sequenziali nel 2011. 
 
 
+#Qui esploriamo come un individuo Fisher ( Pekania pennanti ) che vive nello stato di New York si è spostato nel suo ambiente locale per un periodo di nove notti sequenziali nel 2011. 
+
+data("fisher_ud")   
+fisher_ud
+#class      : RasterStack 
+#dimensions : 176, 177, 31152, 9  (nrow, ncol, ncell, nlayers)
+#resolution : 25, 25  (x, y)
+#extent     : -2282.343, 2142.657, 5100266, 5104666  (xmin, xmax, ymin, ymax)
+#crs        : +proj=moll +lon_0=-73.4137066015374 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs 
+#names      :      night1,      night2,      night3,      night4,      night5,      night6,      night7,      night8,      night9 
+#min values :           0,           0,           0,           0,           0,           0,           0,           0,           0 
+#max values : 0.004695207, 0.007591029, 0.006749434, 0.002891691, 0.002833876, 0.004711692, 0.002017082, 0.002700729, 0.005282948 
 
 
+#Estraiamo le informazione dal Rasterstack
+m2<-metrics_pull(fisher_ud)
+m2
+
+#Creiamo la palette ma questa volta usiamo la funzione palette_timeline perchè a differenza dell'esempio precedente, descrivono una sequenza lineare e non ciclica (ad esempio l'annuale)
+pal2<-palette_timeline(fisher_ud)
+head(pal2)
+
+#Creiamo le mappe (9, una per ogni giorno) che ci descriveranno la posizione dell'individuo in ogni giorno
+map_multiples(m2,ncol = 3, pal2)
+#Capita spesso che le mappe risultino sbiadite.
+#Il parametro lambda_i in map_multiples()e map_single()consente agli utenti di regolare visivamente i pesi relativi dei valori di alta e bassa intensità.
+#In questo caso quindi vogliamo ridurre la disparità tra valori di alta e bassa intensità quindi impostiamo il valore a -5.
+#vedremo che più basso sarà il valore di lambda_i e più sarà intenso il colore
+map_multiples(m2,ncol = 3, pal2, lambda_i = -5)
+
+#A questo punto possiamo distillare le informazioni 
+#È importante ricordare che i valori di specificità (e la conseguente visualizzazione dei valori di specificità) per Fisher devono essere interpretati in modo diverso rispetto a Field Sparrow. 
+#Qui, i valori di specificità indicano il grado in cui l'individuo ha utilizzato le stesse posizioni all'interno del paesaggio per nove notti. Una bassa specificità suggerisce un uso coerente nel tempo. 
+
+m2_distill<-metrics_distill(fisher_ud)
+map_single(m2_distill,pal2,lambda_i = -5)
+
+#Ovviamento avendo utilizzato valori lineari, utilizzeremo legend_timeline e non legend_timecycle
+#in questo caso possiamo utilizzare time_labels per determinare il periodo di indagine.
+legend_timeline(pal2,time_labels = c("2 aprile", "11 aprile"))
 
 
 
@@ -129,15 +166,48 @@ legend_timecycle(pal, origin_label = "jan 1")
 
 #ESEMPIO 3: MAPPARE LE DISTRIBUZIONI DI PIU' INDIVIDUI DURANTE LO STESSO PERIODO DI TEMPO
 
+#Negli esempi precedenti, abbiamo esplorato le distribuzioni di una singola specie e di un singolo individuo in più periodi di tempo. 
+#colorist può essere utilizzato anche per esplorare la distribuzione di più specie o individui in un unico periodo di tempo.
 
+#Qui, utilizziamo i dati di tracciamento GPS raccolti dagli elefanti africani ( Loxodonta africana ) nel Parco nazionale di Etosha (Namibia) durante il 2011 per esplorare come due individui hanno utilizzato il paesaggio nel corso di un anno.
 
+#Carichiamo i dati
 data("elephant_ud")
 elephant_ud
-met2<-metrics_pull((elephant_ud))
-pal2<-palette_set(2)
-map_multiples(met2, pal2, ncol = 2,lambda_i = -5,labels = names(elephant_ud))
-#//
-met2_distt<-metrics_distill(elephant_ud)
-map_single(met2_distt,pal2,lambda_i = -5)
-legend_set(pal2, group_labels = names(elephant_ud))
+#class      : RasterBrick 
+#dimensions : 208, 193, 40144, 2  (nrow, ncol, ncell, nlayers)
+#resolution : 500, 500  (x, y)
+#extent     : -58642.18, 37857.82, -2376938, -2272938  (xmin, xmax, ymin, ymax)
+#crs        : +proj=moll +lon_0=15.8231920275931 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs 
+#source     : memory
+#names      :      LA11,      LA14 
+#min values :         0,         0 
+#max values : 0.8525854, 1.0000000 
+
+
+#Estraiamo i dati dal Rasterstack
+met3<-metrics_pull((elephant_ud))
+
+#Qui incontriamo un'altra funzione, palette_set()
+#Utilizziamo questa funzione quando abbiamo un insieme non ordinato (non rientrano in sequenze lineari o cicliche)
+pal3<-palette_set(elephant_ud)
+
+#Creiamo le mappe multiple e quindi vedremo i due esemplari, usiamo labels per estrarre i nomi prsenti nel Rasterstack
+map_multiples(met3, pal3, ncol = 2,lambda_i = -5,labels = names(elephant_ud))
+
+#Le distribuzioni di utilizzo per LA11 e LA14 sembrano notevolmente simili a prima vista, il che non sorprende dato che i due elefanti appartengono alla stessa mandria. 
+#Tuttavia, possiamo comprendere più chiaramente le somiglianze e le differenze nel modo in cui hanno utilizzato lo spazio nel 2011 “distillando” le informazioni distribuzionali dai due livelli raster con metrics_distill()e visualizzando le metriche risultanti con map_single().
+
+#DOVREMO RICALIBRARE LA NOSTRA COMPRENSIONE DEL SIGNIFICATO DI SPECIFICITA'
+#Ci troviamo in contrasto con gli esempi Field Sparrow e Fisher, dove la specificità indicava il grado in cui i valori di intensità erano incoerenti (o coerenti) nel tempo.
+#QUI POSSIAMO DEFINIRLO COME MISURA DIFFERENZIALE DEL PAESAGGIO.
+#QUINDI BASSA SPECIFICITA' EQUIVALE AD UTILIZZO COMUNE DEL PAESAGGIO, ALTA SPECIFICITA' INDICA UN USO DIFFERENZIALE DEL PAESAGGIO
+
+
+met3_distt<-metrics_distill(elephant_ud)
+map_single(met3_distt,pal2,lambda_i = -5)
+
+#Qui utilizzeremo una legenda diversa ancora una volta.
+#utilizzeremo legend_set coerentemente con il codice precedente
+legend_set(pal3, group_labels = names(elephant_ud))
 
