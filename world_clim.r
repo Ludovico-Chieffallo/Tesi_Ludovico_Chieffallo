48c011c6e51a6960f4c2d5520a04e7614822d619
Ludovico Chieffallo <79975866+Ludovico-Chieffallo@users.noreply.github.com>
2021-10-27 16:55:38 +0200

    Create world_clim.r

---
+++ world_clim.r
@@ -0,0 +1,19 @@
+library(raster)
+getData()
+  
+  
+Worldclim<-raster::getData('worldclim', var='bio', res=10) #Valid resolutions are 0.5, 2.5, 5, and 10 (minutes of a degree). In the case of res=0.5, you must also provide a lon and lat argument for a tile
+
+plot(Worldclim)
+
+
+envData<-crop(Worldclim, extent(-12, 25, 36, 60))
+
+plot(envData$bio1)
+envData
+
+b <- as(extent(envData), "SpatialPolygons")
+
+  
+  
+myCRS<-"+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0" 
