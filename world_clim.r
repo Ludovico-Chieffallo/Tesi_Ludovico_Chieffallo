a988742099bb8447441e9074937b275c9974a472
Elisa Marchetto <62661809+elisamarchetto@users.noreply.github.com>
2021-11-02 16:19:16 +0100

    Update world_clim.r

--- world_clim.r
+++ world_clim.r
@@ -1,13 +1,20 @@
 library(raster)
-getData()
+library(rnaturalearth)
+library(tidyverse)
+library(dplyr)
   
   
-Worldclim<-raster::getData('worldclim', var='bio', res=10) #Valid resolutions are 0.5, 2.5, 5, and 10 (minutes of a degree). In the case of res=0.5, you must also provide a lon and lat argument for a tile
+Worldclim<-raster::getData('worldclim', var='bio', res=2.5) #Valid resolutions are 0.5, 2.5, 5, and 10 (minutes of a degree). In the case of res=0.5, you must also provide a lon and lat argument for a tile
 
-plot(Worldclim)
+# envData<-crop(Worldclim, extent(-12, 25, 36, 60)) approssimativo
 
+Russia <- Europe %>% 
+       filter(
+        str_detect(name_long, 'Russia')
+        ) %>% 
+       select(name_long, geometry) %>% 
+       {. ->> russia})
+envData<-crop(Worldclim, Europe)
+plot(envData)
+EuropePred <- mask(envData, Europe)
 
-envData<-crop(Worldclim, extent(-12, 25, 36, 60))
-
-plot(envData$bio1)
-envData
