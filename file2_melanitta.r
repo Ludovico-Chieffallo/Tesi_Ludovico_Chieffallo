229183b70fd71c6195456229d153931e266f3fd7
Ludovico Chieffallo <79975866+Ludovico-Chieffallo@users.noreply.github.com>
2022-01-22 20:03:54 +0100

    Update file2_melanitta.r

--- file2_melanitta.r
+++ file2_melanitta.r
@@ -160,3 +160,7 @@ view(sdmData)
 FavModel_mela<-multGLM(sdmData_mela, sp.cols = 3, var.cols=4:22, family = "binomial",step = FALSE, Y.prediction = TRUE, P.prediction = TRUE, Favourability = TRUE)
 
 FavModel
+
+#Getpred mela----
+EuropePred <- stack(EuropePred) # it needs to be RasterStack, EuropePred is RasterBrick
+FavPred_mela<- getPreds(EuropePred, models=FavModel_mela$models, id.col = NULL, Y = FALSE, P = FALSE, Favourability = TRUE)
